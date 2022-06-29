c86cac12fb4fd8466624db087e1977f423fdef5d
notaddtry <comatose.9999@mail.ru>
2022-06-30 01:59:51 +0300

    updated styling

--- components/Card/CreditCard.jsx
+++ components/Card/CreditCard.jsx
@@ -7,7 +7,7 @@ import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import Box from '@mui/material/Box'
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from '@mui/material/CircularProgress'
 
 import { isDisabled, reset, validation } from './../../lib/utils/validation'
 import { useEffectFocus } from '../../hooks/useEffectFocus'
@@ -73,113 +73,114 @@ const CreditCard = () => {
 
   return (
     <>
-    {isFormSubmitted && <Box className={styles.loader_wrapper}>
-      <CircularProgress />
-    </Box>}
-    <div className={styles.wrapper}>
-      
-      <Card className={`${styles.content}`} sx={{ mb: 1.5 }}>
-        <CardContent className={styles.card}>
-          <Typography variant='h5' className={styles.card_info_wrapper}>
-            Card Number:
-            <br />
-            <span className={styles.number}>{cardInfo.cardNumber}</span>
-          </Typography>
-          <Typography sx={{ mb: 1.5 }}>
-            Expiration Date:
-            <br />
-            <span className={styles.date}> {cardInfo.expirationDate}</span>
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} className={styles.cvv}>
-            CVV:
-            <br />
-            {cardInfo.cvv}
-          </Typography>
-          <Box className={styles.logo}>
-            <Image
-              src='/images/image.png'
-              width='120'
-              height='40'
-              layout='fixed'
-            />
-          </Box>
-        </CardContent>
-      </Card>
-      <Box sx={{ mb: 1.5 }} className={styles.inputs_wrapper}>
-        <InputMask
-          mask='9999 9999 9999 9999'
-          value={cardInfo.cardNumber}
-          maskChar=''
-          onChange={(e) => handleInput(e)}
-          onBlur={(e) => handleBlur(e)}>
-          {() => (
-            <TextField
-              id='outlined-basic'
-              label='Card Number'
-              variant='outlined'
-              name='cardNumber'
-              inputRef={cardNumberRef}
-              error={cardError.cardNumberError}
-            />
-          )}
-        </InputMask>
-        <InputMask
-          mask='99/9999'
-          value={cardInfo.expirationDate}
-          maskChar=''
-          onChange={(e) => handleInput(e)}
-          onBlur={(e) => handleBlur(e)}>
-          {() => (
-            <TextField
-              id='outlined-basic'
-              label='Expiration Date'
-              variant='outlined'
-              name='expirationDate'
-              inputRef={expirationDateRef}
-              error={cardError.expirationDate}
-              helperText={cardError.expirationDate && 'Date must be real!'}
-            />
-          )}
-        </InputMask>
-        <InputMask
-          mask='999'
-          value={cardInfo.cvv}
-          maskChar=''
-          onChange={(e) => handleInput(e)}
-          onBlur={(e) => handleBlur(e)}>
-          {() => (
-            <TextField
-              id='outlined-basic'
-              label='CVV'
-              variant='outlined'
-              name='cvv'
-              inputRef={cvvRef}
-              error={cardError.cvv}
-            />
-          )}
-        </InputMask>
-      </Box>
-      <Box className={styles.sec_content}>
-        <TextField
-          value={cardInfo.amount}
-          id='outlined-basic'
-          label='Amount'
-          variant='outlined'
-          type='text'
-          inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-          name='amount'
-          inputRef={amountRef}
-          onChange={(e) => handleInput(e)}
-        />
-        <Button
-          disabled={isDisabled(cardInfo) || isFormSubmitted}
-          variant='contained'
-          className={styles.button_buy}
-          onClick={handleSubmit}>
-          Оплатить
-        </Button>
-      </Box>
-    </div>
+      {isFormSubmitted && (
+        <Box className={styles.loader_wrapper}>
+          <CircularProgress />
+        </Box>
+      )}
+      <div className={styles.wrapper}>
+        <Card className={`${styles.content}`} sx={{ mb: 1.5 }}>
+          <CardContent className={styles.card}>
+            <Typography variant='h5' className={styles.card_info_wrapper}>
+              Card Number:
+              <br />
+              <span className={styles.number}>{cardInfo.cardNumber}</span>
+            </Typography>
+            <Typography>
+              Expiration Date:
+              <br />
+              <span className={styles.date}> {cardInfo.expirationDate}</span>
+            </Typography>
+            <Typography className={styles.cvv}>
+              CVV:
+              <br />
+              {cardInfo.cvv}
+            </Typography>
+            <Box className={styles.logo}>
+              <Image
+                src='/images/image.png'
+                width='120'
+                height='40'
+                layout='fixed'
+              />
+            </Box>
+          </CardContent>
+        </Card>
+        <Box sx={{ mb: 1.5 }} className={styles.inputs_wrapper}>
+          <InputMask
+            mask='9999 9999 9999 9999'
+            value={cardInfo.cardNumber}
+            maskChar=''
+            onChange={(e) => handleInput(e)}
+            onBlur={(e) => handleBlur(e)}>
+            {() => (
+              <TextField
+                id='outlined-basic'
+                label='Card Number'
+                variant='outlined'
+                name='cardNumber'
+                inputRef={cardNumberRef}
+                error={cardError.cardNumberError}
+              />
+            )}
+          </InputMask>
+          <InputMask
+            mask='99/9999'
+            value={cardInfo.expirationDate}
+            maskChar=''
+            onChange={(e) => handleInput(e)}
+            onBlur={(e) => handleBlur(e)}>
+            {() => (
+              <TextField
+                id='outlined-basic'
+                label='Expiration Date'
+                variant='outlined'
+                name='expirationDate'
+                inputRef={expirationDateRef}
+                error={cardError.expirationDate}
+                helperText={cardError.expirationDate && 'Date must be real!'}
+              />
+            )}
+          </InputMask>
+          <InputMask
+            mask='999'
+            value={cardInfo.cvv}
+            maskChar=''
+            onChange={(e) => handleInput(e)}
+            onBlur={(e) => handleBlur(e)}>
+            {() => (
+              <TextField
+                id='outlined-basic'
+                label='CVV'
+                variant='outlined'
+                name='cvv'
+                inputRef={cvvRef}
+                error={cardError.cvv}
+              />
+            )}
+          </InputMask>
+        </Box>
+        <Box className={styles.sec_content}>
+          <TextField
+            value={cardInfo.amount}
+            id='outlined-basic'
+            label='Amount'
+            variant='outlined'
+            type='text'
+            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+            name='amount'
+            inputRef={amountRef}
+            onChange={(e) => handleInput(e)}
+          />
+          <Button
+            disabled={isDisabled(cardInfo) || isFormSubmitted}
+            variant='contained'
+            className={styles.button_buy}
+            onClick={handleSubmit}>
+            Оплатить
+          </Button>
+        </Box>
+      </div>
     </>
   )
 }
